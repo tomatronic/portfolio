1e47e18c1c08b72551df6d25d7913845f0baaf33
tomatronic <tom.spencer@rakuten.com>
2021-08-18 17:08:42 +0100

    Updated 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,27 @@
 import NavBar from "./components/navbar"
+import Footer from "./components/footer"
+import Image from "next/image"
+import Link from "next/link"
+import styles from "./css/404.module.css"
 
 export default function Custom404() {
     return (
     <>
     <NavBar />
-    <h1>404 - Page Not Found - Sorry 'bout that</h1>
+    <div className="mainContainer">
+      <div className="smlContainer">
+        <div className={styles.imgSpacer}>
+          <h1>You seem to be lost!</h1>
+          <Image src="/404.png" layout="intrinsic" width="632" height="287" alt="This page cannot be found - Image for 404 page." />
+          <div className={styles.btnCenter}>
+            <Link href="/"><button>Take me Home</button></Link>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <Footer />
+    
     </>
     )
   }
