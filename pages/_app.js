caaa68e4795560a456c94866f6136d2b014a19ac
tomatronic <tom.spencer@rakuten.com>
2021-11-23 13:39:54 +0000

    Tues - Trying to add head details

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 import "../styles.css"
+import Head from "next/head"
 
 // This default export is required in a new `pages/_app.js` file.
 function MyApp({ Component, pageProps }) {
   return (
+    <>
+    <div>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="keywords" content="UX, User Experience, UI, User Interface, Tom Spencer, UK, Brighton" />
+        <meta name="author" content="Tom Spencer" />
+      </Head>
+    </div>
     <Component {...pageProps} />
-
+    </>
   )
 }
 
