3a4720cee702f75c27f46abfcf9be0c65ccdd300
tomatronic <tom.spencer@rakuten.com>
2022-01-01 17:58:46 +0000

    Sat - Working on designflows page, added new pngs mainly with minor layout adjustments

--- pages/work/designflows.js
+++ pages/work/designflows.js
@@ -48,7 +48,7 @@ function caseStudy() {
                             <p>For example I may need to put some shelves up, I would visit this app and find a location to pick a toolkit up containing required tools from a specified pick up location.</p>
                             <p>The brief specified 3 specific pages that required designing:</p>
                             <p>&bull; <b>Toolkit rental</b> - This had to include a search bar<br />
-                                &bull; <b>My rentals</b> - Manage and rack bookings<br />
+                                &bull; <b>My rentals</b> - Manage and track bookings<br />
                                 &bull; <b>How it works</b> - Instructions on how to collect a toolkit</p>
                             <p>As well as an app icon and a poster that presented the screens.</p>
                         </div>
@@ -59,19 +59,26 @@ function caseStudy() {
                             <p>This project was going to be a lot of work, and there was not much time to do it. I decided to spend the friday evening reading through the brief multiple times, I printed the document off and highlighted key points.</p>
                             <p>Once happy I understood what was being asked for I begun making a to do list. This would help me work methodically and would make sure I covered everything required of me during this weekend.</p>
                         </div>
+                        <div className="column">
+                            <Image src="/poa.png" layout="responsive" width="944" height="944" alt="It was important for me to make a to do list to help ensure I stayed on track." />
+                        </div>
                     </div> 
                     <div className="row">
                         <div className="column">
                             <h1>Competitor research</h1>
                             <p>Due to the time constraints I had to take a slightly different appraoch to what I am used to. The research phase is the most important part of the project, it helps ensure that my understanding as as good as it can be.</p>
                             <p>I decided the quickest approach to the research phase of this particular project would be to look at what could be considered competitor businesses. I begun by looking at large DIY style companies, IKEA, B&amp;Q etc. I then also considered businesses that primarily offer a hire service, for example Uber. I then went even further and looked at companies that offered collection and delivery services such as deliveroo. These companies and there apps helped me to build a better understanding of the current market, the common UI elements and where I may be able to make improvements.</p>
+                            <br />
+                            <Image src="/compResearch.png" layout="responsive" width="2000" height="944" alt="Identifying key components in competitor / similar applications allowed me to begun gathering a list of potential components that would be needed." />
                         </div>
-                    </div> 
+                    </div>
                     <div className="row">
                         <div className="column">
                             <h1>Brand identity</h1>
                             <p>I decided to spend some time inventing a brand identiy for this project. It meant I was able to create a simple pattern library that I could then deploy much quicker into the final design.</p>
                             <p>I mainly focused on colour palette and typography. I settled on something that was fairly simple but offered a level of uniqueness.</p>
+                            <br />
+                            <Image src="/brandID.png" layout="responsive" width="2000" height="1040" alt="Identifying key components in competitor / similar applications allowed me to begun gathering a list of potential components that would be needed." />
                         </div>
                     </div>
                     <div className="row">
@@ -80,7 +87,7 @@ function caseStudy() {
                             <p>Now I had a basic pattern library I decided to start sketching out some ideas for my app.</p>
                             <p>I had a lot of ideas at this stage and ended up working out a full user flow, even though I would only be working on a couple of screens, this likely cost some time, but it meant I identified some extra details I could use to enhance the app.</p>
                             <p>I begun with some really rough sketches that I could then iterate on if I felt they had enough potential. This allowed me to complete a lot more sketches in a much shorter amount of time.</p>
-                            <p>By saturday evening I was confident that I had created a sketch that I could work with. I decided to take a break at this point until the next day. It meant I had plenty of time to focus on the high fidelity mocks, icon and presentation poster the next day.</p>
+                            <p>By Saturday evening I was confident that I had created a sketch that I could work with. I decided to take a break at this point until the next day. It meant I had plenty of time to focus on the high fidelity mocks, icon and presentation poster the next day.</p>
                         </div>
                     </div> 
                     <div className="row">
@@ -95,13 +102,17 @@ function caseStudy() {
                         <p>The last major part of the project to complete on sunday afternoon was the app icon. This was an area of the project I struggled with. I have not had to design an app icon before which meant there was a bit of learning curve.</p>
                         <p>The initial step here was to make sure I was working to correct dimensions. I then spent some time understanding what was needed to create an app icon that stands out.</p>
                         <p>I spent a lot of effort and made many iterations to icon. I ended up settling on a simple icon that used the colour palette from the app to ensure a consistant expereince.</p>
+                        <br />
+                        <Image src="/iconexploration.png" layout="responsive" width="2000" height="646" alt="Examples of the many icon ideas I explored" />
+                    Add in example of final icon
                     </div>
                 </div>
                 <div className="row">
-                    <div className="column">
-                        <Image src="/dfFinal.png" layout="responsive" width="1403" height="931" alt="Designflows final submission 2021" />
-                        <br /><br/>
+                    <div className="column">                        
                         <h1>Outcome</h1>
+                        <br />
+                        <Image src="/dfFinal.png" layout="responsive" width="1403" height="931" alt="Designflows final submission 2021" />
+                        <br />
                         <p>Overall I am happy with the result. However, I wish I had more time to perfect this.</p>
                         <p>The initial area I would have spent more time on would have been the research phase, I would have liked to have gained a better understanding of the competitor landscape. The addition of user interviews would have also assisted in the research and the end result. I also think I could of come up with a more refined color palette and framework for the project, but this may of been a stretch to achieve given the time constraint.</p>
                         <br />
