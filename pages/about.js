cee0dd820b61afafb6b128f80a143b5a14374a9f
tomatronic <tom.spencer@rakuten.com>
2022-01-08 17:22:59 +0000

    Added title meta

--- pages/about.js
+++ pages/about.js
@@ -5,55 +5,33 @@ import Footer from "../components/footer"
 
 function About() {
     return <>
-        <NavBar />
-        <div className="mainContainer">
-            <div className={styles.mainContainer}>
-                <div className="reverse-row">
-                    <div className="column">
-                        <img src="/bio.jpeg" alt="You will often find me out hiking in the South Downs National Park" className={styles.bioImg} />
-                    </div>
-                    <div className="double-column">
-                        <h2>
-                            Hello!
-                        </h2>
-                        <p>I'm Tom Spencer, a user experience designer based just outside of sunny Brighton, UK.</p>
-                        <p>Being a UX designer allows me to solve complex problems and bring the end user a pleasurable experience. I make sure that products are enjoyable, accessible and beautiful to look at. I have a passion for getting fully immersed in a problem and experimenting with a range of possible solutions.</p>
-                        <p>Seeing the impact of how I can influence a user's behaviour within a product or application fascinates me. Being empathetic and understanding helps ensure that I can make a product not just work for its intended users but be something they feel comfortable with.</p>
-                        <p>I'm always looking for opportunities for collaboration. <a href="mailto:tom.m.spencer+portfolio@gmail.com">Reach out and let's chat!</a></p>
-                        {/* <button className={styles.btnintro}>Download Resume</button> */}
+        <div>
+            <Head>
+                <title>Tom Spencer - UX/UI Designer</title>
+                <meta property="og:title" content="Tom Spencer - Ux / UI Designer" />
+            </Head>
+            <NavBar />
+            <div className="mainContainer">
+                <div className={styles.mainContainer}>
+                    <div className="reverse-row">
+                        <div className="column">
+                            <img src="/bio.jpeg" alt="You will often find me out hiking in the South Downs National Park" className={styles.bioImg} />
+                        </div>
+                        <div className="double-column">
+                            <h2>
+                                Hello!
+                            </h2>
+                            <p>I'm Tom Spencer, a user experience designer based just outside of sunny Brighton, UK.</p>
+                            <p>Being a UX designer allows me to solve complex problems and bring the end user a pleasurable experience. I make sure that products are enjoyable, accessible and beautiful to look at. I have a passion for getting fully immersed in a problem and experimenting with a range of possible solutions.</p>
+                            <p>Seeing the impact of how I can influence a user's behaviour within a product or application fascinates me. Being empathetic and understanding helps ensure that I can make a product not just work for its intended users but be something they feel comfortable with.</p>
+                            <p>I'm always looking for opportunities for collaboration. <a href="mailto:tom.m.spencer+portfolio@gmail.com">Reach out and let's chat!</a></p>
+                        </div>
                     </div>
                 </div>
-                {/* <div className="row">
-                    <div className="column">
-                        <h2>Education</h2>
-                        <p>Bachelor of Arts - Digital Media Design | 2011</p>
-                    </div>
-                    <div className="column">
-                        <h2>Experience</h2>
-                        <p>Rakuten Advertising | Feb 2020 - Present <br />
-                            User Experience Designer
-                        </p>
-                        <p>
-                            Rakuten Advertising | June 2018 - Feb 2020 <br />
-                            Junior User Experience Designer
-                        </p>
-                        <p>
-                            Pure360 | Feb 2016 - Jun 2018 <br />
-                            Senior Creative Developer
-                        </p>
-                        <p>
-                            Pure360 | Jun 2014 - Feb 2016 <br />
-                            Creative Developer
-                        </p>
-                        <p>Friday Media Group | Nov 2014 - Apr 2014 <br />
-                            Email Marketing Creator
-                        </p>
-                    </div>
-                </div> */}
             </div>
+            <Footer />
         </div>
-        <Footer />
     </>
-  }
-  
-  export default About
+}
+
+export default About
--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Head from "next/head"
 
 function HomePage() {
 
-  
+
   React.useEffect(() => {
     const path = window.location.hash
     if (path && path.includes("#")) {
@@ -23,78 +23,78 @@ function HomePage() {
     }
   })
 
-    return <>
-  <div>
-  <Head>
-    <title>Tom Spencer - UX/UI Designer</title>
-    <meta property="og:title" content="Tom Spencer - Ux / UI Designer" />
-  </Head>
-    <NavBar />
-    <div className={styles.mainIntroContainer}>
-      <div className={styles.introContainer}>
-        <div className="top-row-intro">
-          <div className="column-image">
-            <div className={styles.introImg}>
-              <img src="/tomspencer.png" alt="Tom Spencer - UX/UI Designer" width="120" />
+  return <>
+    <div>
+      <Head>
+        <title>Tom Spencer - UX/UI Designer</title>
+        <meta property="og:title" content="Tom Spencer - Ux / UI Designer" />
+      </Head>
+      <NavBar />
+      <div className={styles.mainIntroContainer}>
+        <div className={styles.introContainer}>
+          <div className="top-row-intro">
+            <div className="column-image">
+              <div className={styles.introImg}>
+                <img src="/tomspencer.png" alt="Tom Spencer - UX/UI Designer" width="120" />
+              </div>
+            </div>
+            <div className="column">
+              <div className={styles.blurb}>Hi, I'm</div>
+              <div className={styles.intro}>Tom Spencer.</div>
+              <div className={styles.blurb}>
+                <br /><div className={styles.highlight}>UX/UI designer
+                </div>
+                &nbsp;focused on delivering <div className={styles.keyPoint}><span>effective</span></div> user centric designs that drive engagement.<br /><br />Currently UX Designer @ <b>Rakuten Advertising</b>.<br /><br /><br />
+              </div>
+
+              <div className={styles.btnContainer}>
+                <Link href="#work"><button className={styles.btnintro}>See my case studies</button></Link>
+              </div>
+              <br />
+              <i>You can also check out some of my UI projects on <a href="https://dribbble.com/tomatronic" target="_blank" rel="noreferrer" >Dribbble</a> and my professional achievements on <a href="https://www.linkedin.com/in/thomas-spencer/" target="_blank" rel="noreferrer">Linkedin</a>.</i>
             </div>
           </div>
-          <div className="column">
-            <div className={styles.blurb}>Hi, I'm</div>
-            <div className={styles.intro}>Tom Spencer.</div>
-            <div className={styles.blurb}>
-          <br /><div className={styles.highlight}>UX/UI designer
-          </div>
-             &nbsp;focused on delivering <div className={styles.keyPoint}><span>effective</span></div> user centric designs that drive engagement.<br /><br />Currently UX Designer @ <b>Rakuten Advertising</b>.<br/><br /><br />
-        </div>
-      
-        <div className={styles.btnContainer}>
-          <Link href="#work"><button className={styles.btnintro}>See my case studies</button></Link>
-        </div>
-        <br />
-        <i>You can also check out some of my UI projects on <a href="https://dribbble.com/tomatronic" target="_blank" rel="noreferrer" >Dribbble</a> and my professional achievements on <a href="https://www.linkedin.com/in/thomas-spencer/" target="_blank" rel="noreferrer">Linkedin</a>.</i>
-          </div>
         </div>
       </div>
-      </div>      
       <a name="work" id="work" />
       <div className="mainContainer">
-      <div className="row">          
+        <div className="row">
           <div className="column">
-          <Link href="/work/designflows"><img src="/169competition.png" alt="Designflows 2021 - Coming Soon" className="imgRound" /></Link>
+            <Link href="/work/designflows"><img src="/169competition.png" alt="Designflows 2021 - Coming Soon" className="imgRound" /></Link>
           </div>
           <div className="column">
-          <div className={styles.csTitle}>Designflows 2021</div>
-              <h3>Tool Hire App</h3>
-              <p>A competition project to create a unique UI within a short amount of time.</p>
-              <Link href="/work/designflows"><button className="secondary">View Case Study &#x2192;</button></Link>
+            <div className={styles.csTitle}>Designflows 2021</div>
+            <h3>Tool Hire App</h3>
+            <p>A competition project to create a unique UI within a short amount of time.</p>
+            <Link href="/work/designflows"><button className="secondary">View Case Study &#x2192;</button></Link>
           </div>
+        </div>
+        <div className="row">
+          <div className="column">
+            <Link href="/work/offer-management"><img src="/169offers.png" alt="Offer management section of the new dashboard for Rakuten Advertising" className="imgRound" /></Link>
+          </div>
+          <div className="column">
+            <div className={styles.csTitle}>Rakuten Advertising</div>
+            <h3>Offer Management</h3>
+            <p>The updating of a legacy application to offer a modern experience for the user.</p>
+            <Link href="/work/offer-management"><button className="secondary">View Case Study &#x2192;</button></Link>
+          </div>
+        </div>
+        <div className="row">
+          <div className="column">
+            <Link href="/work/brewtiful"><img src="/169brewtiful.png" alt="Brewtiful - My own project looking at beers and design" className="imgRound" /></Link>
+          </div>
+          <div className="column">
+            <div className={styles.csTitle}>Brewtiful</div>
+            <h3>Beer Blog</h3>
+            <p>A personal project where I set out to capture a range of beers and breweries and talk about both the beer itself and the can (and bottle) designs.</p>
+            <button className="secondary" disabled>Coming Soon...</button>
+          </div>
+        </div>
       </div>
-      <div className="row">          
-          <div className="column">
-          <Link href="/work/offer-management"><img src="/169offers.png" alt="Offer management section of the new dashboard for Rakuten Advertising" className="imgRound" /></Link>
-          </div>
-          <div className="column">
-          <div className={styles.csTitle}>Rakuten Advertising</div>
-              <h3>Offer Management</h3>
-              <p>The updating of a legacy application to offer a modern experience for the user.</p>
-              <Link href="/work/offer-management"><button className="secondary">View Case Study &#x2192;</button></Link>
-          </div>
-      </div>
-      <div className="row">          
-          <div className="column">
-          <Link href="/work/brewtiful"><img src="/169brewtiful.png" alt="Brewtiful - My own project looking at beers and design" className="imgRound" /></Link>
-          </div>
-          <div className="column">
-          <div className={styles.csTitle}>Brewtiful</div>
-              <h3>Beer Blog</h3>
-              <p>A personal project where I set out to capture a range of beers and breweries and talk about both the beer itself and the can (and bottle) designs.</p>
-              <button className="secondary" disabled>Coming Soon...</button>
-          </div>
-      </div>
-      </div>
-    <Footer />
+      <Footer />
     </div>
-    </>   
-  }
-  
-  export default HomePage
+  </>
+}
+
+export default HomePage
