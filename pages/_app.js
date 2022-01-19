24770fa64c21bb5fcf6b951142719d94c6a82f9e
tomatronic <tom.spencer@rakuten.com>
2022-01-19 14:34:09 +0000

    Added animation to home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,23 @@
 import "../styles.css";
+import { useEffect } from "react";
 import Script from "next/script";
+import { useRouter } from "next/router";
 import Head from "next/head";
+import * as gtag from "../lib/gtag";
 
 // This default export is required in a new `pages/_app.js` file.
 function MyApp({ Component, pageProps }) {
+  
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <>
@@ -11,7 +25,7 @@ function MyApp({ Component, pageProps }) {
     
 <Script
   strategy="lazyOnload"
-  src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+  src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
 />
 <Script strategy="lazyOnload">
         {`
@@ -19,7 +33,9 @@ function MyApp({ Component, pageProps }) {
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          gtag('config', '${gtag.GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
+          });
         `}
       </Script>
 
